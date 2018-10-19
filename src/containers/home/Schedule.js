4b9f820606a1e7883bcc39b787fc93977ad88286
zlatan <zlatan.pham@gmail.com>
2018-10-19 22:51:31 +0700

    workshop

--- src/containers/home/Schedule.js
+++ src/containers/home/Schedule.js
@@ -7,58 +7,138 @@ const events = [
     title: 'Registration',
   },
   {
+    highlighted: false,
+    time: '08:45',
+    title: 'Welcome',
+  },
+  {
     highlighted: true,
     time: '09:00',
-    title: 'Registration',
+    title: 'Opening Keynote',
   },
   {
     highlighted: false,
     time: '09:45',
     title: 'Starting Serverless with Go',
     desc:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus cursus eleifend. In imperdiet mi ac faucibus efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos',
+      'Serverless – so hot right now. All the big players of cloud platforms have launched some kind of runtime to execute code. By focusing on coding business logic this leads to short production-ready developments. This talk aims to show why Go is a good choice to start with serverless.',
     speaker: {
       avatar:
         'https://papercallio-production.s3.amazonaws.com/uploads/user/avatar/35028/TRIODOS_STM_RGB_7414_closeup.jpg',
       name: 'Stefan Meissner',
-      desc: 'Erlang for GO developers',
+      desc: 'Backend developer',
     },
   },
   {
     highlighted: false,
-    time: '10:00',
+    time: '10:30',
     title: 'Building Resilient Data Pipelines in Go',
+    desc:
+      'The modern world runs on Data. In this talk we will cover how Gophers of any level can easily build Data Pipelines in Go with Kafka and Cassandra. At the end, we will look at how GE has written a Data Pipeline in Go that can handle over 800,000 writes per second of industrial time series data.',
     speaker: {
       avatar:
-        'https://papercallio-production.s3.amazonaws.com/uploads/user/avatar/35028/TRIODOS_STM_RGB_7414_closeup.jpg',
-      name: 'Stefan Meissner',
-      desc: 'Erlang for GO developers',
+        'https://secure.gravatar.com/avatar/f982183e1db9eed435f2097d10281f7c?s=500',
+      name: 'Grant Griffiths',
+      desc: 'Senior software engineer',
     },
   },
   {
     highlighted: false,
-    time: '20:45',
+    time: '11:00',
+    title: 'Genetic Algorithms in Go',
+    desc:
+      "Genetic algorithms are metaheuristics that are based on the process of natural selection. This talk give a simple introduction to GA, with a couple of examples including evolving a phrase from Shakespeare's Hamlet, and also an image of Mona Lisa.",
+    speaker: {
+      avatar:
+        'https://secure.gravatar.com/avatar/ee191858f0d96ad93098694537f71998?s=500',
+      name: 'Sau Sheong Chang',
+      desc: 'Software engineer expert',
+    },
+  },
+  {
+    highlighted: false,
+    time: '11:30',
+    title: 'Lunch',
+  },
+  {
+    highlighted: false,
+    time: '13:00',
+    title: 'Golang - Metric tracking and Nuances of Memory Management',
+    desc:
+      "Golang, we all know that it is a super fast, super developer friendly language.  But with all this speed around Golang it's very common for developers to suffer from pitfalls. I am gonna talk about how we can avoid those.",
+    speaker: {
+      avatar:
+        'https://secure.gravatar.com/avatar/3692ed4ac12d1d367148d6c7afab5908?s=500',
+      name: 'Deepesh naini',
+      desc: 'Polygot developer',
+    },
+  },
+  {
+    highlighted: false,
+    time: '13:45',
     title: 'Concurrency in Ethereum (Blockchain)',
     speaker: {
       avatar:
-        'https://papercallio-production.s3.amazonaws.com/uploads/user/avatar/35028/TRIODOS_STM_RGB_7414_closeup.jpg',
-      name: 'Stefan Meissner',
+        'https://secure.gravatar.com/avatar/3692ed4ac12d1d367148d6c7afab5908?s=500',
+      name: 'Tu Nguyen',
       desc: 'Erlang for GO developers',
     },
+  },
+  {
+    highlighted: false,
+    time: '14:30',
+    title: 'Black Box Monitoring in Go',
+    desc:
+      "You've set up your favorite monitoring agent on all of your services and you have incredible visibility into the internals of your infrastructure. All seems good in the world, but can your end user actually use your service? In this talk we'll write a simple app that simulates end user activity.",
+    speaker: {
+      avatar:
+        'https://secure.gravatar.com/avatar/f982183e1db9eed435f2097d10281f7c?s=500',
+      name: 'Grant Griffiths',
+      desc: 'Senior software engineer',
+    },
+  },
+  {
+    highlighted: false,
+    time: '15:00',
+    title: 'Why would anyone embed Javascript into the Go app? (we do)',
+    desc:
+      "Golang is nice, fast and static, everyone knows that. Why would you even want to embed Javascript into that perfectly tuned Go machine? I'm going to tell how we process data with the rules written in Javascript in our Go app (and it works surprisingly fine).",
+    speaker: {
+      avatar:
+        'https://secure.gravatar.com/avatar/5141bce120e3b351e2a5156366bc8a24?s=500',
+      name: 'Aleksei Shaikhaleev',
+      desc: 'Backend developer',
+    },
+  },
+  {
+    highlighted: false,
+    time: '15:45',
+    title: 'Resiliency in Distributed Systems',
+    desc:
+      'Keeping distributed systems up and running is a hard problem to solve at scale. Adopting some basic patterns can help us guard our systems from sudden spike in traffic, dependency failures, network issues, slower downstream services and can help us achieve considerable uptime for our systems.',
+    speaker: {
+      avatar:
+        'https://secure.gravatar.com/avatar/15a0a233d3bf86fea7bbaa525f33897d?s=500',
+      name: 'Rajeev N Bharshetty',
+      desc: 'Computer science nerd',
+    },
+  },
+  {
+    highlighted: false,
+    time: '16:30',
+    title: 'Closing',
   },
 ]
 
 const ScheduleSection = () => (
   <section
     id="schedule"
-    className="bg-grey-lightest sm:pt-20 pt-10 sm:pb-40 pb-32"
+    className="bg-grey-lightest sm:pt-24 pt-10 sm:pb-40 pb-32"
   >
     <div className="container pb-8r">
       <div className="row flex items-center flex-wrap">
         <div className="col w-full">
-          <h2 className="text-primary text-3xl text-center mb-16">
-            Conference
-          </h2>
+          <h2 className="text-primary text-3xl text-center mb-16">Schedule</h2>
 
           <div className="timeline">
             {events.map(
--- src/containers/home/Workshop.js
+++ src/containers/home/Workshop.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
-const ScheduleSection = () => (
-  <section
-    id="schedule"
-    className="bg-grey-lightest sm:pt-20 pt-10 sm:pb-40 pb-32"
-  >
+const WorkshopSection = () => (
+  <section id="workshop" className="bg-primary sm:pt-24 pt-10 sm:pb-32 pb-24">
     <div className="container pb-8r">
       <div className="row flex items-center flex-wrap">
         <div className="col w-full">
-          <h2 className="text-primary text-3xl text-center mb-16">Workshop</h2>
+          <h2 className="text-3xl text-center mb-16 text-white">Workshop</h2>
 
-          <div className="timeline">
+          <div className="timeline text-white">
             <div className="row flex">
               <div className="col timeline__time flex flex-col items-center">
-                <span className="text-2xl mb-3">08:00</span>
-                <div className="timeline__line bg-primary flex-1" />
+                <span className="text-2xl mb-3 text-white">08:00</span>
+                <div className="timeline__line bg-white opacity-50 flex-1" />
               </div>
               <div className="col">
                 <h4 className="text-xl mt-1 mb-16">{'Registration'}</h4>
@@ -22,20 +19,20 @@ const ScheduleSection = () => (
             </div>
             <div className="row flex">
               <div className="col timeline__time flex flex-col items-center">
-                <span className="text-2xl font-bold text-primary mb-3 mt-3">
+                <span className="text-2xl font-bold text-white mb-3 mt-3">
                   09:00
                 </span>
-                <div className="timeline__line bg-primary flex-1" />
+                <div className="timeline__line bg-white opacity-50 flex-1" />
               </div>
               <div className="col mb-16">
                 <h4 className="text-xl mt-3 mb-4">Go Fundamentals</h4>
-                <p className="mb-4">
+                <p className="mb-4 opacity-75">
                   This class provides an idiomatic introduction to the Go
                   programming language supported by extensive examples and
                   hands-on exercises
                 </p>
                 <h5 className="text-base mb-2">Course online</h5>
-                <ul>
+                <ul className="opacity-75">
                   <li>Language Syntax</li>
                   <li>Using Pointers</li>
                   <li>Arrays, Slices and Maps</li>
@@ -49,28 +46,26 @@ const ScheduleSection = () => (
             </div>
             <div className="row flex">
               <div className="col timeline__time flex flex-col items-center">
-                <span className="text-2xl my-3">12:00</span>
-                <div className="timeline__line bg-primary flex-1" />
+                <span className="text-2xl my-3 text-white">12:00</span>
+                <div className="timeline__line bg-white opacity-50 flex-1" />
               </div>
               <div className="col">
-                <h4 className="text-xl mt-4 mb-16">Lunch break</h4>
+                <h4 className="text-xl mt-4 mb-16 text-white">Lunch break</h4>
               </div>
             </div>
             <div className="row flex">
               <div className="col timeline__time flex flex-col items-center">
-                <span className="text-2xl font-bold text-primary mb-3 mt-3">
-                  13:30
-                </span>
-                <div className="timeline__line bg-primary flex-1" />
+                <span className="text-2xl font-bold mb-3 mt-3">13:30</span>
+                <div className="timeline__line bg-white opacity-50 flex-1" />
               </div>
               <div className="col">
-                <h4 className="text-xl mt-3 mb-4">Go Immediates</h4>
+                <h4 className="text-xl mt-3 mb-4 text-white">Go Immediates</h4>
                 <p className="mb-4">
                   This is for any intermediate-level developer who has some
                   experience with other programming languages and wants to learn
                   Go.
                 </p>
-                <ul>
+                <ul className="opacity-75">
                   <li>Language Syntax</li>
                   <li>Data Structures</li>
                   <li>Interface</li>
@@ -81,10 +76,50 @@ const ScheduleSection = () => (
               </div>
             </div>
           </div>
+
+          <h2 className="text-center text-white mt-20 mb-10">
+            About the instructor
+          </h2>
+          <div className="mx-auto lg:w-1/2 text-center">
+            <div
+              className="avatar bg-cover bg-no-repeat bg-center rounded-full mx-auto"
+              style={{
+                width: 160,
+                height: 160,
+                backgroundImage: `url(https://secure.gravatar.com/avatar/ee191858f0d96ad93098694537f71998?s=500`,
+              }}
+            />
+            <h4 className="text-white text-xl mt-6 mb-4">Sau Sheong Chang</h4>
+            <p className="text-white">
+              Sau Sheong has been doing software development for 22 years,
+              mostly in web application development. He is active in the Ruby
+              and Go developer communities have have contributed to open source
+              projects and spoke at meetups and conferences. Sau Sheong has also
+              published 4 programming-related books, on Ruby and Go.
+            </p>
+          </div>
+
+          <h3 className="text-center text-white mt-20 mb-10">Venues</h3>
+          <div className="mx-auto lg:w-4/5 text-white">
+            <div className="row flex text-center flex-wrap">
+              <div className="col sm:w-1/3 w-full mb-10">
+                <h5 className="text-xl mb-2">Hoa Sen University</h5>
+                <p className="text-lg">8 Nguyen Van Trang, D1</p>
+              </div>
+              <div className="col sm:w-1/3 w-full mb-10">
+                <h5 className="text-xl mb-2">RMIT University</h5>
+                <p className="text-lg">702 Nguyen Van Linh, D7</p>
+              </div>
+              <div className="col sm:w-1/3 w-full mb-10">
+                <h5 className="text-xl mb-2">HCMUS</h5>
+                <p className="text-lg">227 Nguyen Van Cu, D5</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
 )
 
-export default ScheduleSection
+export default WorkshopSection
