7f2a2d847135f62190b89f146290a683cab3cb3f
Hai Nguyen <nghai.dev@gmail.com>
2019-10-07 18:28:17 +0700

    Update Sponsors

--- artifacts/react-static-browser-plugins.js
+++ artifacts/react-static-browser-plugins.js
@@ -1,19 +1,19 @@
 // Imports
-import plugin0 from '/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-reach-router/browser.api.js'
+import plugin0 from '/Users/ng-hai/SourcesCode/gophercon-landing/node_modules/react-static-plugin-reach-router/browser.api.js'
 
 // Plugins
 const plugins = [{
-        location: "/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-reach-router",
+        location: "/Users/ng-hai/SourcesCode/gophercon-landing/node_modules/react-static-plugin-reach-router",
         plugins: [],
         hooks: plugin0({})
       },
 {
-        location: "/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-favicons",
+        location: "/Users/ng-hai/SourcesCode/gophercon-landing/node_modules/react-static-plugin-favicons",
         plugins: [],
         hooks: {}
       },
 {
-        location: "/Users/ngochangjelly/Desktop/gophercon-landing",
+        location: "/Users/ng-hai/SourcesCode/gophercon-landing",
         plugins: [],
         hooks: {}
       }]
