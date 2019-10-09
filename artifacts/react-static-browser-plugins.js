dc55430defca52340931f3cb39294d058d919a57
ngochangjelly <ngochang29397@gmail.com>
2019-10-09 11:08:52 +0700

    update remote

--- artifacts/react-static-browser-plugins.js
+++ artifacts/react-static-browser-plugins.js
@@ -1,26 +1,22 @@
 // Imports
-import plugin0 from '/Users/ldhnam/development/gophercon-landing/node_modules/react-static-plugin-reach-router/browser.api.js'
+import plugin0 from '/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-reach-router/browser.api.js'
 
 // Plugins
-const plugins = [
-  {
-    location:
-      '/Users/ldhnam/development/gophercon-landing/node_modules/react-static-plugin-reach-router',
-    plugins: [],
-    hooks: plugin0({}),
-  },
-  {
-    location:
-      '/Users/ldhnam/development/gophercon-landing/node_modules/react-static-plugin-favicons',
-    plugins: [],
-    hooks: {},
-  },
-  {
-    location: '/Users/ldhnam/development/gophercon-landing',
-    plugins: [],
-    hooks: {},
-  },
-]
+const plugins = [{
+        location: "/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-favicons",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/ngochangjelly/Desktop/gophercon-landing",
+        plugins: [],
+        hooks: {}
+      }]
 
 // Export em!
 export default plugins
--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,10 +1,11 @@
+
+
+  
 // Template Map
 export default {
-  '/Users/ldhnam/development/gophercon-landing/src/pages/404': require('/Users/ldhnam/development/gophercon-landing/src/pages/404')
-    .default,
-  '/Users/ldhnam/development/gophercon-landing/src/pages/home': require('/Users/ldhnam/development/gophercon-landing/src/pages/home')
-    .default,
+  '/Users/ngochangjelly/Desktop/gophercon-landing/src/pages/404': require('/Users/ngochangjelly/Desktop/gophercon-landing/src/pages/404').default,
+'/Users/ngochangjelly/Desktop/gophercon-landing/src/pages/home': require('/Users/ngochangjelly/Desktop/gophercon-landing/src/pages/home').default
 }
 
-export const notFoundTemplate =
-  '/Users/ldhnam/development/gophercon-landing/src/pages/404'
+export const notFoundTemplate = '/Users/ngochangjelly/Desktop/gophercon-landing/src/pages/404'
+
