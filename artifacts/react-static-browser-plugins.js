dc6f92d633ccaa7652525c3f4294dfa9465b80e9
ldhnam <namhoai95@gmail.com>
2019-10-12 14:58:49 +0700

    Remove become a sponsor

--- artifacts/react-static-browser-plugins.js
+++ artifacts/react-static-browser-plugins.js
@@ -1,19 +1,19 @@
 // Imports
-import plugin0 from '/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-reach-router/browser.api.js'
+import plugin0 from '/Users/ldhnam/development/gophercon-landing/node_modules/react-static-plugin-reach-router/browser.api.js'
 
 // Plugins
 const plugins = [{
-        location: "/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-reach-router",
+        location: "/Users/ldhnam/development/gophercon-landing/node_modules/react-static-plugin-reach-router",
         plugins: [],
         hooks: plugin0({})
       },
 {
-        location: "/Users/ngochangjelly/Desktop/gophercon-landing/node_modules/react-static-plugin-favicons",
+        location: "/Users/ldhnam/development/gophercon-landing/node_modules/react-static-plugin-favicons",
         plugins: [],
         hooks: {}
       },
 {
-        location: "/Users/ngochangjelly/Desktop/gophercon-landing",
+        location: "/Users/ldhnam/development/gophercon-landing",
         plugins: [],
         hooks: {}
       }]
